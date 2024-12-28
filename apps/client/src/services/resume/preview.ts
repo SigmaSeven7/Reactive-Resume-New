36a9973048da9e8bef04ee2a1ed540c0cb347c4c
Oryan Tayer <oryantayer@gmail.com>
2024-12-28 18:19:19 +0200

    add preview

--- apps/client/src/services/resume/preview.ts
+++ apps/client/src/services/resume/preview.ts
@@ -1,24 +1,67 @@
+// import { UrlDto } from "@reactive-resume/dto";
+// import { useQuery } from "@tanstack/react-query";
+
+// import { RESUME_PREVIEW_KEY } from "@/client/constants/query-keys";
+// import { axios } from "@/client/libs/axios";
+
+// export const previewResume = async (data: { id: string }) => {
+//   const response = await axios.get<UrlDto>(`/resume/print/${data.id}/preview`);
+
+//   return response.data;
+// };
+
+// export const useResumePreview = (id: string) => {
+//   const {
+//     error,
+//     isPending: loading,
+//     data,
+//   } = useQuery({
+//     queryKey: [RESUME_PREVIEW_KEY, { id }],
+//     queryFn: () => previewResume({ id }),
+//   });
+
+//   return { url: data?.url, loading, error };
+// };
+
+
 import { UrlDto } from "@reactive-resume/dto";
 import { useQuery } from "@tanstack/react-query";
+import { ResumeData } from "@reactive-resume/schema";
+import { useCallback, useEffect, useRef } from "react";
 
 import { RESUME_PREVIEW_KEY } from "@/client/constants/query-keys";
 import { axios } from "@/client/libs/axios";
 
 export const previewResume = async (data: { id: string }) => {
   const response = await axios.get<UrlDto>(`/resume/print/${data.id}/preview`);
-
   return response.data;
 };
 
-export const useResumePreview = (id: string) => {
-  const {
-    error,
-    isPending: loading,
-    data,
-  } = useQuery({
-    queryKey: [RESUME_PREVIEW_KEY, { id }],
-    queryFn: () => previewResume({ id }),
-  });
+export const useResumePreview = (resume: ResumeData) => {
+  const frameRef = useRef<HTMLIFrameElement>(null);
 
-  return { url: data?.url, loading, error };
+  const updateResumeInFrame = useCallback(() => {
+    if (!frameRef.current?.contentWindow) return;
+
+    const message = {
+      type: "SET_RESUME",
+      payload: resume
+    };
+
+    (() => {
+      frameRef.current.contentWindow.postMessage(message, "*");
+    })();
+  }, [frameRef, resume]);
+
+  useEffect(() => {
+    if (!frameRef.current) return;
+
+    frameRef.current.addEventListener("load", updateResumeInFrame);
+
+    return () => {
+      frameRef.current?.removeEventListener("load", updateResumeInFrame);
+    };
+  }, [frameRef, updateResumeInFrame]);
+
+  return frameRef;
 };
